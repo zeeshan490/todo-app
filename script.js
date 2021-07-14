3b1a580f4b9e01521aecf4861cd141589feb4468
zeeshan ajmal <zeeshanajmal8@gmail.com>
2021-07-14 16:32:06 +0530

    started todo

---
+++ script.js
@@ -0,0 +1,9 @@
+let addTodoButton = document.querySelector(".add-todo");
+//selected this button with the selector
+
+addTodoButton.addEventListener("click",function(){
+    console.log("Add Todo Button Clicked");
+})
+
+//this will attach event on addtodobutton 
+// click is inbuild keyword we cant change it , ther r many like it
