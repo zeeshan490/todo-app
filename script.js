9f307a958990e70efaa45188790d82818422ef86
zeeshan ajmal <zeeshanajmal8@gmail.com>
2021-07-14 17:20:08 +0530

    click and keypress buttons live

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 let addTodoButton = document.querySelector(".add-todo");
 //selected this button with the selector
+let todoInput = document.querySelector(".todo-input");
 
-addTodoButton.addEventListener("click",function(){
-    console.log("Add Todo Button Clicked");
-})
+
+todoInput.addEventListener("keypress", function(e){
+    if(e.key=="Enter"){
+        addTodo();
+    }
+});
+addTodoButton.addEventListener("click", function(){
+    addTodo();
+});
+
+function addTodo(){
+    let todoInputvalue = todoInput.value;
+    if(todoInputvalue){
+        console.log(todoInputvalue);
+        todoInput.value ="";
+    }
+}
+
+
+
+ 
+// function addTodo(e){         //this fn will get called every time click event executed
+//     let todoInputvalue = todoInput.value;   //get the input from .value
+
+//     if(todoInputvalue){    //only when input is non empty
+//         console.log(todoInputvalue);
+
+//         todoInput.value =""; //it will empty the todo input
+//     }
+// }
 
 //this will attach event on addtodobutton 
 // click is inbuild keyword we cant change it , ther r many like it
+
